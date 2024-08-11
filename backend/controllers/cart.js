b6b8e9af820208e15aeb36d0beb033c3eb7cfa7b
Anjali singh <singhanzali98@gmail.com>
2024-08-11 23:13:31 +0545

    getcartadded

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -33,9 +33,22 @@ const removebookfromcart = asyncHandler(async (req, res) => {
          return res.status(500).json({ message: "Server Error", error: error.message }); // Provide error details
     }
 });
+const getcartbook = asyncHandler(async (req, res) => {
+    try {
+        // Fetch the user from the database using the ID stored in the JWT
+        const { id } = req.headers;
+        const userdata = await User.findById(id).populate("favourites");
+        const favouriteBooks = userdata.favourites;
+        
+        return res.status(200).json({ status: "Success", data:favouriteBooks });
+    } catch (error) {
+        console.error("Error in adding favrourites books in controller:", error); // Log detailed error
+         return res.status(500).json({ message: "Server Error", error: error.message }); // Provide error details
+    }
+});
 
 
 
 
 
-module.exports = { addtocart,removebookfromcart}
+module.exports = { addtocart,removebookfromcart,getcartbook}
