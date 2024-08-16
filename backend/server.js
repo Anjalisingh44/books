4cd858783c27b364fd2f26f0aa495c9f67ae6c6c
Anjali singh <singhanzali98@gmail.com>
2024-08-16 09:09:59 +0545

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require("cors")
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/dbconnection");
 connectDb();
 const app = express();
 
 const port = process.env.PORT ||5000;
+app.use(cors(
+    {
+        origin: 'http://localhost:5173', // Allow requests from this origin
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'bookid']
+    }
+));
 app.use(express.json());
 app.use("/api/users", require("./routes/user"));
 app.use("/api/users", require("./routes/book"));
