bccb6aa08c90196a987c948accb4adedbdb4db9d
Anjali singh <singhanzali98@gmail.com>
2024-08-16 09:13:18 +0545

    All books added

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios';
+import Bookcard from '../components/Bookcard/Bookcard';
+
 
 const AllBooks = () => {
+  const [Data, setData] = useState([]);
+
+  useEffect(() => {
+      const fetch = async () => {
+          try {
+              const response = await axios.get("http://localhost:1000/api/users/get-all-book");
+              setData(response.data.data);
+          } catch (error) {
+              console.error("Error fetching data:", error);
+          }
+      };
+      fetch();
+  }, []);
   return (
-    <div>AllBooks</div>
-  )
-}
+    <div className='bg-zinc-900 px-12 py-8 h-auto'>
+      <h4 className='text-2xl font-semibold text-yellow-100'>All books</h4>
+      <div className='my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8'>
+        {Data.length > 0 ? (
+          Data.map((item, index) => (
+            <div  className=" text-white" key={index}>
+              <Bookcard data={item} />
+            </div>
+          ))
+        ) : (
+          <p className='text-yellow-100'>No books available</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default AllBooks
