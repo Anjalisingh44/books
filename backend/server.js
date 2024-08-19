6192a6bbb2f4679d187bd151f49a9287159a85ca
Anjali singh <singhanzali98@gmail.com>
2024-08-19 19:55:14 +0545

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,11 @@ connectDb();
 const app = express();
 
 const port = process.env.PORT ||5000;
-app.use(cors(
-    {
-        origin: 'http://localhost:5173', // Allow requests from this origin
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'bookid']
-    }
-));
+app.use(cors({
+    origin: 'http://localhost:5173', // The origin of your frontend application
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Ensure 'Authorization' is included
+  }));
 app.use(express.json());
 app.use("/api/users", require("./routes/user"));
 app.use("/api/users", require("./routes/book"));
