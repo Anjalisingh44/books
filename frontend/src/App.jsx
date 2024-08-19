daf86dae1fef141c7fef2dc5dad25bf47231cefb
Anjali singh <singhanzali98@gmail.com>
2024-08-19 19:55:38 +0545

    App.js uddated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import { Routes,Route} from "react-router-dom";
 import AllBooks from './pages/AllBooks';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Cart from './pages/Cart';
 import ViewBookDetails from './components/ViewBookDetails/ViewBookDetails';
+import { useDispatch, useSelector } from 'react-redux';
+import { authAction } from './store/auth';
 const App = () => {
+  const dispatch = useDispatch();
+  const role = useSelector((state)=> state.auth.role);
+  useEffect(() =>{
+    if(
+      localStorage.getItem("id") &&
+      localStorage.getItem("accessToken") && 
+      localStorage.getItem("role")
+    ){
+      dispatch(authAction.SignIn());
+      dispatch(authAction.changeRole(localStorage.getItem("role")));
+    }
+  })
   return (
     <div>
-      <Router> <Navbar/>
+       <Navbar/>
       <Routes>
         <Route exact path='/' element={<Home />}></Route>
         <Route  path='/all-books' element={<AllBooks />}></Route>
@@ -23,7 +37,7 @@ const App = () => {
         <Route path="view-book-details/:id" element= {<ViewBookDetails />} />
       </Routes>
       <Footer/>
-      </Router>
+      
       
    
     
