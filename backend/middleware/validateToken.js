f4c2565c72ccd977c4c96844a5805942c4f50349
Anjali singh <singhanzali98@gmail.com>
2024-08-23 11:00:37 +0545

    expire date token increased

--- backend/middleware/validateToken.js
+++ backend/middleware/validateToken.js
@@ -3,25 +3,30 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
-  let authHeader = req.headers.Authorization || req.headers.authorization;
+  let authHeader = req.headers.authorization || req.headers.Authorization;
 
   if (authHeader && authHeader.startsWith("Bearer ")) {
     token = authHeader.split(" ")[1];
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
-        res.status(401);
-        throw new Error("User is not authorized");
+        if (err.name === 'TokenExpiredError') {
+          res.status(401).json({ message: "Token expired, please log in again" });
+        } else {
+          res.status(401).json({ message: "User is not authorized" });
+        }
+      } else {
+        req.user = { 
+          id: decoded.user.id, 
+          email: decoded.user.email, 
+          username: decoded.user.username, 
+          role: decoded.user.role 
+        }; // Attach the decoded payload to the request object
+        next(); // Call next() to pass the request to the next middleware or route handler
       }
-      req.user = { id: decoded.user.id, 
-        email: decoded.user.email, 
-        username: decoded.user.username, 
-        role: decoded.user.role };// Attach the decoded payload to the request object
-      next(); // Call next() to pass the request to the next middleware or route handler
     });
   } else {
-    res.status(401);
-    throw new Error("User is not authorized or token is missing");
+    res.status(401).json({ message: "User is not authorized or token is missing" });
   }
 });
 
