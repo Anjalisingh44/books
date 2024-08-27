f5040965beb72110642d7b3774fdb5f065cdd566
Anjali singh <singhanzali98@gmail.com>
2024-08-27 20:39:52 +0545

    user added

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,7 +20,7 @@ const userSchema = new Schema({
     },
     avatar: {
         type: String,
-        default: "https://images.app.goo.gl/N7qLtMtvMVA3p6mY7",
+        default: "https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg",
     },
     role: {
         type: String,
