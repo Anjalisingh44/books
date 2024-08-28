601ae1b09813e43b93442ffc291f327531fccbd0
Anjali singh <singhanzali98@gmail.com>
2024-08-28 23:03:06 +0545

    updt

--- frontend/src/components/Profile/Sidebar.jsx
+++ frontend/src/components/Profile/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AiOutlineLogout } from "react-icons/ai";
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import { authAction } from '../../store/auth';
 
 
@@ -10,7 +10,8 @@ import { authAction } from '../../store/auth';
 const Sidebar = ({data}) => {
   const dispatch = useDispatch();
    const navigate = useNavigate();
-  
+   const role = useSelector((state) => state.auth.role);
+   const isSiggnedIn = useSelector((state) => state.auth.isSiggnedIn); 
   return (
     <div className='bg-zinc-800 p-3 rounded flex flex-col items-center justify-between h-[100%] '>
       <div className='p-7 flex  items-center flex-col justify-center '>{" "}
@@ -18,7 +19,8 @@ const Sidebar = ({data}) => {
       <p className='mt-2 text-xl text-zinc-100 font-semibold '>{data.username}</p>
       <p className='mt-2 text-normal text-zinc-300'>{data.email}</p>
       <div className='w-full mt-4 h-[1px] bg-zinc-500 hidden lg:block'>  </div>
-     
+     {role === "user" &&  (
+      
       <div className='w-full flex flex-col items-center justify-center hidden lg:flex '>
         <Link  
         to='/profile/favourites'
@@ -38,6 +40,22 @@ const Sidebar = ({data}) => {
 
       </div>
      
+     )}
+     {role === "admin" && (
+       <div className='w-full flex flex-col items-center justify-center hidden lg:flex '>
+       <Link  
+       to='/profile/all-order'
+       className='text-zinc-100 font-semibold w-full mt-20 text-center hover:bg-zinc-900 rounded transistion-all '
+       > All Orders
+       </Link>
+       <Link  
+       to='/profile/add-book'
+       className='text-zinc-100 font-semibold w-full py-4 text-center hover:bg-zinc-900 rounded transistion-all mt-2'
+       > Add Book
+       </Link>
+     </div>
+
+     )}
      <button className='bg-zinc-900 w-3/6 lg:w-full mt-4 lg:mt-20 text-white font-semibold flex items-center justify-center py-2 rounded hover:bg-white hover:text-zinc-900 transistion-all duration-300 ' onClick={() => {
       dispatch(authAction.logout());
       dispatch(authAction.changeRole("user"));
